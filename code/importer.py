0b3087055b38b51c16ba8b95548f67099fc47b06
Alexandru HTC <alexandru.htc@abwesend.de>
2026-03-30 15:59:09 +0200

    added separator

--- code/importer.py
+++ code/importer.py
@@ -3,11 +3,14 @@ import pathlib
 from pyspark.sql import DataFrame, SparkSession
 
 
-def import_my_csv(name:str, spark:SparkSession)->DataFrame:
+def import_my_csv(name: str, spark: SparkSession) -> DataFrame:
     """import a csv file and return a DataFrame"""
     pth = "../resources/" + name
     assert pathlib.Path(pth).exists(), f"File not found: {pth}"
-    abs_path = pathlib.Path(pth).resolve()
+    abs_path: str = str(pathlib.Path(pth).resolve())
     print(f"Importing file from: {abs_path}")
-    df = spark.read.csv(str(abs_path), header=True, inferSchema=True)
+    df = spark.read.csv(abs_path,
+                        header=True,
+                        inferSchema=True,
+                        sep=";")
     return df
