d882ddfde30d539e696d3744886f555b2726102a
Alexandru HTC <alexandru.htc@abwesend.de>
2026-03-30 15:51:58 +0200

    using absolut path

--- code/importer.py
+++ code/importer.py
@@ -7,6 +7,7 @@ def import_my_csv(name:str, spark:SparkSession)->DataFrame:
     """import a csv file and return a DataFrame"""
     pth = "../resources/" + name
     assert pathlib.Path(pth).exists(), f"File not found: {pth}"
-
-    df = spark.read.csv(name, header=True, inferSchema=True)
+    abs_path = pathlib.Path(pth).resolve()
+    print(f"Importing file from: {abs_path}")
+    df = spark.read.csv(str(abs_path), header=True, inferSchema=True)
     return df
